2298bc0a77bd950d66d0ed15b5bc7da41745d98e
Viktor Svyatokha <ahydrax@gmail.com>
2019-05-06 23:30:48 +1000

    Fixed tests

--- tests/Hangfire.PostgreSql.Tests/ExpirationManagerFacts.cs
+++ tests/Hangfire.PostgreSql.Tests/ExpirationManagerFacts.cs
@@ -211,6 +211,6 @@ values ('key', 1, now() at time zone 'utc' - interval '{0} seconds') returning "
 
         private static string GetSchemaName() => ConnectionUtils.GetSchemaName();
 
-        private static ExpirationManager CreateManager() => new ExpirationManager(ConnectionUtils.GetConnectionProvider(), TimeSpan.Zero);
+        private static ExpirationManager CreateManager() => new ExpirationManager(ConnectionUtils.GetConnectionProvider(), TimeSpan.FromMilliseconds(1));
     }
 }
