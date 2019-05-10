714cbb3cc08c15ac23cdd078f964818b104aa78c
Viktor Svyatokha <ahydrax@gmail.com>
2019-05-11 03:16:23 +1000

    Added missing method

--- src/Hangfire.PostgreSql/Connectivity/ConnectionHolderExtensions.cs
+++ src/Hangfire.PostgreSql/Connectivity/ConnectionHolderExtensions.cs
@@ -17,6 +17,15 @@ namespace Hangfire.PostgreSql.Connectivity
             CommandType? commandType = null)
             => connectionHolder.Connection.Execute(sql, param, transaction, commandTimeout, commandType);
 
+        public static T Fetch<T>(this ConnectionHolder connectionHolder,
+            string sql,
+            object param = null,
+            IDbTransaction transaction = null,
+            int? commandTimeout = null,
+            CommandType? commandType = null)
+            where T : struct
+            => connectionHolder.Connection.QueryFirstOrDefault<T?>(sql, param, transaction, commandTimeout, commandType) ?? default(T);
+
         public static List<T> FetchList<T>(this ConnectionHolder connectionHolder,
             string sql,
             object param = null,
