64f7376055c9171f91ce5ad13bac1c9ff782be2e
Viktor Svyatokha <ahydrax@gmail.com>
2019-05-11 23:48:34 +1000

    Removed unused field

--- src/Hangfire.PostgreSql/Entities/SqlHash.cs
+++ src/Hangfire.PostgreSql/Entities/SqlHash.cs
@@ -6,7 +6,6 @@ namespace Hangfire.PostgreSql.Entities
     [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
     internal class SqlHash
     {
-        public long Id { get; set; }
         public string Key { get; set; }
         public string Field { get; set; }
         public string Value { get; set; }
