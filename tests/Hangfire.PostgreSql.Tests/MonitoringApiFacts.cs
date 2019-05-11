9de5bd9eb055f8648ebaeb7d1dc03ceeed549994
Viktor Svyatokha <ahydrax@gmail.com>
2019-05-11 23:14:45 +1000

    Added tests

--- tests/Hangfire.PostgreSql.Tests/MonitoringApiFacts.cs
+++ tests/Hangfire.PostgreSql.Tests/MonitoringApiFacts.cs
@@ -3,30 +3,30 @@ using System.Linq;
 using System.Threading;
 using Hangfire.PostgreSql.Connectivity;
 using Hangfire.PostgreSql.Tests.Utils;
+using Hangfire.Server;
 using Hangfire.States;
+using Hangfire.Storage;
 using Xunit;
 
 namespace Hangfire.PostgreSql.Tests
 {
     public class MonitoringApiFacts
     {
-        private readonly IConnectionProvider _connectionProvider;
-        private readonly MonitoringApi _monitoringApi;
+        private readonly IMonitoringApi _monitoringApi;
+        private readonly PostgreSqlStorage _storage;
 
         public MonitoringApiFacts()
         {
-            _connectionProvider = ConnectionUtils.GetConnectionProvider();
-            _monitoringApi = new MonitoringApi(_connectionProvider);
+            _storage = new PostgreSqlStorage(ConnectionUtils.GetConnectionString());
+            _monitoringApi = _storage.GetMonitoringApi();
         }
 
         [Fact, CleanDatabase]
         public void ScheduledCount_ReturnsActualValue()
         {
             // Arrange
-            var storage = new PostgreSqlStorage(ConnectionUtils.GetConnectionString());
-            var backgroundJobClient = new BackgroundJobClient(storage);
+            var backgroundJobClient = new BackgroundJobClient(_storage);
 
-            // Act
             backgroundJobClient.Schedule(
                 () => Worker.DoWork("hello"),
                 TimeSpan.FromSeconds(1));
@@ -34,8 +34,10 @@ namespace Hangfire.PostgreSql.Tests
                 () => Worker.DoWork("hello-2"),
                 TimeSpan.FromSeconds(2));
 
+            // Act
+            var scheduledCount = _storage.GetMonitoringApi().ScheduledCount();
+
             // Assert
-            var scheduledCount = storage.GetMonitoringApi().ScheduledCount();
             Assert.Equal(2, scheduledCount);
         }
 
@@ -43,16 +45,15 @@ namespace Hangfire.PostgreSql.Tests
         public void EnqueuedCount_ReturnsActualValue()
         {
             // Arrange
-            var storage = new PostgreSqlStorage(ConnectionUtils.GetConnectionString());
-            var backgroundJobClient = new BackgroundJobClient(storage);
-
-            // Act
+            var backgroundJobClient = new BackgroundJobClient(_storage);
             backgroundJobClient.Enqueue(() => Worker.DoWork("hello"));
             backgroundJobClient.Enqueue(() => Worker.DoWork("hello-2"));
             backgroundJobClient.Enqueue(() => Worker.DoWork("hello-3"));
 
+            // Act
+            var enqueuedCount = _monitoringApi.EnqueuedCount("default");
+
             // Assert
-            var enqueuedCount = storage.GetMonitoringApi().EnqueuedCount("default");
             Assert.Equal(3, enqueuedCount);
         }
 
@@ -60,18 +61,16 @@ namespace Hangfire.PostgreSql.Tests
         public void FetchedCount_ReturnsActualValue()
         {
             // Arrange
-            var storage = new PostgreSqlStorage(ConnectionUtils.GetConnectionString());
-            var backgroundJobClient = new BackgroundJobClient(storage);
-
-            // Act
+            var backgroundJobClient = new BackgroundJobClient(_storage);
             backgroundJobClient.Enqueue(() => Worker.DoWork("hello"));
             backgroundJobClient.Enqueue(() => Worker.DoWork("hello-2"));
             backgroundJobClient.Enqueue(() => Worker.DoWork("hello-3"));
+            var fetched = _storage.GetConnection().FetchNextJob(new[] { "default" }, CancellationToken.None);
 
-            var fetched = storage.GetConnection().FetchNextJob(new[] { "default" }, CancellationToken.None);
+            // Act
+            var fetchedCount = _monitoringApi.FetchedCount("default");
 
             // Assert
-            var fetchedCount = storage.GetMonitoringApi().FetchedCount("default");
             Assert.Equal(1, fetchedCount);
         }
 
@@ -79,26 +78,46 @@ namespace Hangfire.PostgreSql.Tests
         public void Queues_ReturnsActualQueues()
         {
             // Arrange
-            var storage = new PostgreSqlStorage(ConnectionUtils.GetConnectionString());
-            var backgroundJobClient = new BackgroundJobClient(storage);
-
-            // Act
+            var backgroundJobClient = new BackgroundJobClient(_storage);
             backgroundJobClient.Create(() => Worker.DoWork("hello-1"), new EnqueuedState("default"));
             backgroundJobClient.Create(() => Worker.DoWork("hello-2"), new EnqueuedState("test1"));
             backgroundJobClient.Create(() => Worker.DoWork("hello-3"), new EnqueuedState("test2"));
             backgroundJobClient.Create(() => Worker.DoWork("hello-4"), new EnqueuedState("default"));
-            
-            // Assert
-            var queues = storage.GetMonitoringApi()
+
+            // Act
+            var queues = _monitoringApi
                 .Queues()
                 .Select(x => x.Name)
                 .ToArray();
 
+            // Assert
             Assert.Contains("default", queues);
             Assert.Contains("test1", queues);
             Assert.Contains("test2", queues);
         }
-        
+
+        [Fact, CleanDatabase]
+        public void Servers_ReturnsActualServers()
+        {
+            // Arrange
+            var queues = new[] { "default", "test" };
+            var workerCount = 10;
+            _storage.GetConnection().AnnounceServer("test-server", new ServerContext
+            {
+                Queues = queues,
+                WorkerCount = workerCount
+            });
+
+            // Act
+            var server = _monitoringApi
+                .Servers()
+                .Single();
+
+            Assert.Equal("test-server", server.Name);
+            Assert.Equal(queues, server.Queues);
+            Assert.Equal(workerCount, server.WorkersCount);
+        }
+
         public static class Worker
         {
             public static void DoWork(string argument)
